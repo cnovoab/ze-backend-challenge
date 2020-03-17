ba9469f98e38e7c8f8776271cf004dae77392131
Claudio Novoa <claudio.novoa.b@gmail.com>
2020-03-17 17:53:18 -0300

    feat: add swagger documentation (#4)

--- src/web-server/routes/index.ts
+++ src/web-server/routes/index.ts
@@ -1,5 +1,7 @@
 /* istanbul ignore file */
 import * as express from 'express';
+import * as swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from '../swagger.json';
 import * as healthcheck from 'express-healthcheck';
 import test from './test';
 import partners from './partners';
@@ -9,5 +11,5 @@ const router = express.Router();
 router.use('/test', test);
 router.use('/healthcheck', healthcheck());
 router.use('/partners', partners);
-
+router.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 export default router;
